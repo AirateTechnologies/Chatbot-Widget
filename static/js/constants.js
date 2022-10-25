26d00b4397e18de733137e9546a83b0a09ed9275
Aleksei <alekseiismirnovv@gmail.com>
2022-10-25 19:46:52 +0300

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+const rasa_server_url = "http://188.126.62.18:5001/webhooks/rest/webhook";
 const sender_id = uuidv4();
